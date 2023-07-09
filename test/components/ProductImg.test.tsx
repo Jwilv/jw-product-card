c61020630653a315f3c8222569968564d9a9fcf8
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-07-09 20:01:44 -0300

    ProductImg test

---
+++ test/components/ProductImg.test.tsx
@@ -0,0 +1,17 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import { afterEach, describe, expect, it } from "vitest";
+import { ProductImg } from "../../src/components";
+import React from "react";
+
+
+describe('ProductImg', () => {
+
+    afterEach(cleanup);
+
+    it('render for ProductImg', () => {
+        const wrapper = render(<ProductImg img="img.jpg" />)
+        expect(wrapper).toMatchSnapshot()
+    });
+
+
+});
