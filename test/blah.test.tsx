41831a405368cc656eec4dc1bbb1916225fbbdd4
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-07-09 13:57:43 -0300

    initial test

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,28 @@
-// import * as React from 'react';
+import * as React from 'react';
 // import * as ReactDOM from 'react-dom';
 // import { Thing } from '../src';
 
-// describe('it', () => {
-//   it('renders without crashing', () => {
-//     const div = document.createElement('div');
-//     ReactDOM.render(<Thing />, div);
-//     ReactDOM.unmountComponentAtNode(div);
-//   });
-// });
+import { cleanup, render, screen } from "@testing-library/react";
+import { afterEach, describe, expect, it } from "vitest"
+import { ProductTitle } from '../src/components';
+
+describe('vos sabes', () => {
+
+  afterEach(cleanup)
+
+  it('renders without crashing', () => {
+    expect(true).toBe(true)
+  });
+
+  it('render ProductTitle', () => {
+    const wrapper = render(<ProductTitle title='hola' className='white-text' />)
+    expect(wrapper).toMatchSnapshot()
+  });
+
+  it('render for parameter hello word', () => {
+    render(<ProductTitle title='Hello world' className='white-text' />)
+    const result = screen.getByText('Hello world')
+    expect(result.classList.contains('white-text')).toBe(true)
+  });
+
+});
